61990af24667ee9a1eb48d14bc6b19b5bbcd9ef4
Yixin <yxyxchen@bu.edu>
2019-08-14 16:04:32 -0400

    daily

--- simModelRepitation.R
+++ simModelRepitation.R
@@ -18,25 +18,44 @@ load("genData/expDataAnalysis/blockData.RData")
 # select common useID
 idList = hdrData$ID
 
+# test useID
+decodes = c("RL1", "RL2", "BL", "QL1", "QL2")
+encodes = c("RL2", "QL2")
+for(e in 1 : length(encodes)){
+  encode = encodes[e]
+  # load simData
+  load(sprintf("genData/simulation/%s.RData", encode))
+  ids = hdrData$ID
+  nSub = length(ids)    
+  for(d in 1 : length(decodes)){
+    decode = decodes[d]
+    # determine paraNames
+    paraNames = getParaNames(decode)
+    nPara = length(paraNames)
+    if(paraNames == "wrong model name"){
+      print(paraNames)
+      break
+    }
+    # determine excID
+    expPara = loadExpPara(paraNames,
+                          sprintf("genData/simModelFitting/%s/%sdb", encode, decode))
+    useID = getUseID(expPara, paraNames)
+    excID = ids[!ids %in% useID]
+    txt = sprintf("%s_%s:%s", encode, decode, length(useID))
+    print(txt)
+    readline("continue")
+  }
+}
 
-#
-modelName = "RL2"
+# 
+modelName = "QL2"
 paraNames = getParaNames(modelName)
 nPara = length(paraNames)
-expPara = loadExpPara(paraNames, sprintf("genData/expModelFitting/%sdb", "RL2"))
-simPara = loadExpPara(paraNames, sprintf("genData/simModelFitting/%sdb", "RL2"))
+expPara = loadExpPara(paraNames, sprintf("genData/expModelFitting/%sdb", modelName))
+simPara = loadExpPara(paraNames, sprintf("genData/simModelFitting/%s/%sdb", modelName, modelName))
 expID = getUseID(expPara, paraNames)
 simID = getUseID(simPara, paraNames)
 useID = idList[idList %in% expID & idList %in% simID]
-
-
-tempt  = cbind(exp = as.vector(as.matrix(expPara[,1:nPara])), sim = as.vector(as.matrix(simPara[,1 : nPara])))
-data.frame(tempt, para = factor(rep(paraNames, each = nrow(simPara)), levels = paraNames),
-           id = rep(simPara$id,  nPara)) %>% filter(id %in% useID) %>% 
-  ggplot(aes(exp, sim)) + geom_point() + facet_wrap(.~para, scales = "free") +
-  geom_abline(aes(slope = 1, intercept = 0), color = "red", linetype = "dashed") + 
-  myTheme
-
 
 tempt  = cbind(exp = as.vector(as.matrix(expPara[,1:nPara])), sim = as.vector(as.matrix(simPara[,1 : nPara])))
 data.frame(tempt, para = factor(rep(paraNames, each = nrow(simPara)), levels = paraNames),
@@ -45,4 +64,5 @@ data.frame(tempt, para = factor(rep(paraNames, each = nrow(simPara)), levels = p
   geom_abline(aes(slope = 1, intercept = 0), color = "red", linetype = "dashed") + 
   myTheme
 dir.create("figures/paraRecovery")
-ggsave("figures/paraRecovery/RL2.png", width = 6, height = 4)
+ggsave(sprintf("figures/paraRecovery/%s.png", modelName) ,width = 6, height = 4)
+
